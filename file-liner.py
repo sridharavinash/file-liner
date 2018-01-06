3a62ddfbadca79c66a6b9b42d0c85a04d64b2325
Avinash Sridhar <sridharavinash@github.com>
2018-01-06 16:18:43 -0500

    working example that spits out the start of a line number of comment in file

--- file-liner.py
+++ file-liner.py
@@ -2,6 +2,7 @@
 
 import requests
 import os
+import re
 import argparse
 
 from gql import gql, Client
@@ -22,6 +23,21 @@ GITHUB_URL="https://api.github.com/graphql"
 GITHUB_TOKEN=os.getenv('GITHUB_TOKEN')
 DATA= open("./comments.graphql").read()
 
+# get the first value of the diff hunk line number
+diff_re = re.compile("@@ -(\d+).*@@")
+
+def calc_start_line(diff_hunk):
+    ''' Get the start of the line of the comment from the diffHunk'''
+    global diff_re
+    found_start =  diff_re.search(diff_hunk)
+    if found_start == None:
+        return 0
+    pos = int(found_start[1])
+    for i in diff_hunk.split('\n')[1:]:
+        if i[0] != '-':
+            pos +=1
+    return pos
+
 client = Client(
     transport=RequestsHTTPTransport(url=GITHUB_URL,
                                     headers={
@@ -31,5 +47,13 @@ client = Client(
 )
 query = gql(DATA)
 variables={"owner": owner, "repo": repo, "pr_id": prId}
+
+
 result = client.execute(query, variables)
-print(result)
+
+for r in result['repository']['pullRequest']['reviews']['nodes']:
+    for n in r['comments']['nodes']:
+        print(n['author']['login'])
+        print(n['path'])
+        print(n['bodyText'])
+        print(calc_start_line(n['diffHunk']))
